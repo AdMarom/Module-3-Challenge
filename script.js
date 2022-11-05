8be1f110c50f0a5a0cae2a038e1d1f9ce217d4c2
AdMarom <adenamarom@gmail.com>
2022-11-05 00:04:23 -0700

    input code for number of characters prompt

--- script.js
+++ script.js
@@ -4,27 +4,41 @@
 // Get references to the #generate element
 // var generateBtn = document.querySelector("#generate");
 
+// var x = answer;
+// var moveOn = (x<128);
+// var moveBack = (x>=128);
 
 
 function Btn(){
   var generateBtn = document.querySelector("#generate");
 window.alert ("specify criteria");
-if (generateBtn) {
-  window.prompt("How many characters are in your password?");
-  }
-
+let criteria = window.prompt ("How many characters are in your password?");
+if (criteria <= 128 && criteria >= 8) {
+  window.prompt("Are there upper case letters?");
 }
+else if (criteria > 128) {
+  window.alert("Characters must be less than 128");
+}
+else if (criteria < 8) {
+  window.alert("Characters must be greater than 7");
+}
+
+// else if (moveBack) {
+//   window.alert ("less than 128");
+// }
+}
+
 
 
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
+// }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
